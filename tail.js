62a12d70974491c4553e73e84d693f4334874fd5
henryhe1234 <henryhe1123@gmail.com>
2020-08-18 01:14:38 +0000

    writing tail function

---
+++ tail.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(0x1F600)}Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${String.fromCodePoint(0x1F600)}Assertion Passed: ${actual} !== ${expected}`);
+
+  }
+};
+const tail = function(array) {
+  let answer = [];
+  if (array.length === 1) {
+    return [];
+  } else if (array.length === 0) {
+    return [];
+  } else {
+    for (let i = 1; i < array.length; i++) {
+      answer.push(array[i]);
+    }
+    return answer;
+  }
+};
+console.log(tail([]));
+console.log(tail([1]));
