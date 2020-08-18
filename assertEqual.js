79612e80d732ca3242a977b73454d871ed12435a
henryhe1234 <henryhe1123@gmail.com>
2020-08-18 00:05:44 +0000

    using ES6 Template Literals for string

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(String.fromCodePoint(0x1F600) + "Assertion Passed: " + actual + " === " + expected);
+    console.log(`${String.fromCodePoint(0x1F600)}Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(String.fromCodePoint(0x1F625) + "Assertion failed: " + actual + " !== " + expected);
+    console.log(`${String.fromCodePoint(0x1F600)}Assertion Passed: ${actual} !== ${expected}`);
 
   }
 };
