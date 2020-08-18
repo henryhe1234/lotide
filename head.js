68e231f3b87a50bc32fbfe01b1ae66d5158aff16
henryhe1234 <henryhe1123@gmail.com>
2020-08-18 00:59:05 +0000

    creating head.js

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(0x1F600)}Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${String.fromCodePoint(0x1F600)}Assertion Passed: ${actual} !== ${expected}`);
+
+  }
+};
+const head = function(array){
+  if(array === null){
+    return undefined;
+  }
+  return array[0];
+};
+console.log(head([1]));
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
