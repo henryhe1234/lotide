52360746c356805c53b57c84807eba4b48301366
henryhe1234 <henryhe1123@gmail.com>
2020-08-24 23:21:08 +0000

    modify assertArrayEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,6 @@
-const eqArrays = (array1, array2) => {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-const assertArraysEqual = (array1, array2) => {
+
+const eqArrays = require('./eqArrays');
+assertArraysEqual = (array1, array2) => {
   if (eqArrays(array1, array2)) {
     console.log(`${String.fromCodePoint(0x1F600)}Assertion Passed: ${array1} === ${array2}`);
   } else {
@@ -15,3 +9,4 @@ const assertArraysEqual = (array1, array2) => {
   }
 };
 assertArraysEqual([1, 2, "3"], [3, 2, 1]);
+module.exports = assertArraysEqual;
