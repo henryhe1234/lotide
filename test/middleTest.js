886e31aa5873a8438bd685933e8e3928e7814406
henryhe1234 <henryhe1123@gmail.com>
2020-08-25 03:24:03 +0000

    writing test case for middle and tail function

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,3 +1,15 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
-assertArraysEqual(middle([1,2,3,67]),middle([1,2,3,4]));
+const assert = require('chai').assert;
+
+describe('#middle', () => {
+  it('returns [5] for [1,5,2]', () => {
+    assert.deepEqual(middle([1,5,3]), [5]);
+  });
+  it('should return [2,3] for [1,2,3,4]',()=>{
+    assert.deepEqual(middle([1,2,3,4]),[2,3]);
+  });
+  it('should return [] when pass [1]', ()=> {
+    assert.deepEqual(middle([1]),[]);
+  });
+  
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,3 +1,10 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail');
-assertEqual(tail([5,6,7]).length,2);
+const assert = require('chai').assert;
+
+// assertEqual(tail([5,6,7]).length,2);
+describe('#tail', () => {
+  it('returns [6,7] for [5,2,3]', () => {
+    assert.deepEqual(tail([5,6,7]), [6,7]);
+  });
+  
+});
